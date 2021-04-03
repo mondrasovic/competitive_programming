cf80f189fe01e97e9435d0d6ab334a9bb71b1f1f
mondrasovic <milan.ondrasovic@gmail.com>
2021-04-03 19:55:00 +0200

    Add initial work on the UVA 1026 problem.

---
+++ UVA/problem_1026.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int a1, b1, t1, a2, b2, t;
+    while ((cin >> a1 >> b1 >> t1 >> a2 >> b2 >> t) && (a1 > 0)) {
+
+    }
+
+    return 0;
+}
