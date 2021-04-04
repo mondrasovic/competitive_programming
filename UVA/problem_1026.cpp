e7d4ae0b61590d3793635d420405290dc70063b8
Milan Ondrašovič <milan.ondrasovic@gmail.com>
2021-04-04 18:34:19 +0200

    Implement 'The Solar System' problem. Still not finished.

--- UVA/problem_1026.cpp
+++ UVA/problem_1026.cpp
@@ -1,16 +1,48 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 
 using namespace std;
 
+typedef double n_t;
+
+static const n_t kPi = acos(-1);
+static const n_t kEps = 1.e-5;
+
 int main()
 {
+    freopen("input.txt", "rt", stdin);
+
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int a1, b1, t1, a2, b2, t;
-    while ((cin >> a1 >> b1 >> t1 >> a2 >> b2 >> t) && (a1 > 0)) {
+    n_t a_1, b_1, t1, a_2, b_2, t;
+    int n_case = 0;
+    while ((cin >> a_1 >> b_1 >> t1 >> a_2 >> b_2 >> t) && (a_1 > 0)) {
+        auto t_2 = t1 * pow(a_2 / a_1, 1.5);
+        t = fmod(t, t_2);
 
+        auto target_area = kPi * a_2 * b_2 * (t / t_2);
+        n_t theta_lower = 0, theta_upper = 2 * kPi;
+
+        for (int i = 0; i < 1000; ++i) {
+            auto theta = (theta_lower + theta_upper) / 2;
+            auto scale = a_2 / b_2;
+            auto theta_scaled = atan(scale * tan(theta));
+            auto circle_sector_area = (theta_scaled * a_2 * a_2) / 2;
+            auto ellipse_sector_area = circle_sector_area / scale;
+
+            if (ellipse_sector_area < target_area)
+                theta_lower = theta;
+            else
+                theta_upper = theta;
+        }
+
+        auto x = a_2 * cos(theta_lower);
+        auto y = b_2 * sin(theta_lower);
+
+        cout << "Solar System " << (++n_case) << ": "
+            << fixed << setprecision(3) << x << ' ' << y << endl;
     }
 
     return 0;
